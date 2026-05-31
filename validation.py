24add853ac2305b7eb8f0a921e6a0d343dcbc145
Edwin w.m <edwin.wainaina1@student.moringaschool.com>
2026-05-31 17:54:20 +0300

    Refactor validation functions to simplify return statements and improve readability

--- validation.py
+++ validation.py
@@ -3,16 +3,16 @@ from datetime import datetime
 def validate_task_title(title):
     if len(title) == 0:
         return False
-    else:
-        return True
+    return True
 
 def validate_task_description(description):
     if len(description) == 0:
         return False
-    else:
-        return True
+    return True
 
 def validate_due_date(due_date):
+    if len(due_date) == 0:
+        return False
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
         return True
